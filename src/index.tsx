a0a310fc5620f6c0bfc676176318415bafc9bce7
Emieldv <emielvlees@hotmail.com>
2022-07-21 16:29:27 +0200

    Host font locally

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import "@fontsource/teko";
 import "./normalize.css";
 import "./index.css";
 import { DataProvider } from "./lib/context/Data";
